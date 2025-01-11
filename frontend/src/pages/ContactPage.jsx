bdd047dc0619d225a70189b49944742aef6cabb0
mehaaamajeed <165750437+mehaaamajeed@users.noreply.github.com>
2025-01-11 19:33:42 +0530

    Add files via upload

---
+++ frontend/src/pages/ContactPage.jsx
@@ -0,0 +1,8 @@
+// src/pages/ContactPage.jsx
+import React from "react";
+
+function ContactPage() {
+    return <h1>Contact Us</h1>;
+}
+
+export default ContactPage;
---
+++ frontend/src/pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <div className="welcome-container">
+      <h1><center>Welcome to Haventrace</center></h1>
+      
+          </div>
+  );
+};
+
+export default HomePage;
