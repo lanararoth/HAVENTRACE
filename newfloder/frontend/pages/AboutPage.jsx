6b656e7b20e5cee2fa0e3df02572f1f86709ec05
lanararoth <165750255+lanararoth@users.noreply.github.com>
2025-01-11 19:12:26 +0530

    Delete newfloder directory

--- newfloder/frontend/pages/AboutPage.jsx
+++
@@ -1,8 +0,0 @@
-// src/pages/AboutPage.jsx
-import React from "react";
-
-function AboutPage() {
-    return <h1>About Haventrace</h1>;
-}
-
-export default AboutPage;
--- newfloder/frontend/pages/ContactPage.jsx
+++
@@ -1,8 +0,0 @@
-// src/pages/ContactPage.jsx
-import React from "react";
-
-function ContactPage() {
-    return <h1>Contact Us</h1>;
-}
-
-export default ContactPage;
