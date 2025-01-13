ee9621e5ce243c83bca7c19735d7269ab71fc91b
mehaaamajeed <165750437+mehaaamajeed@users.noreply.github.com>
2025-01-13 15:22:52 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,20 +2,20 @@ import React from 'react';
 import logo from './assets/logo.png';
 import './App.css';
 import { useNavigate } from 'react-router-dom';
-
-
 function App() {
 
   const navigate = useNavigate();
 
-  const navigateToWelcome = () =>{
-    navigate('/welcome')
+  const navigateToHomePage = () =>{
+    navigate('/home')
   }
-  return (
+  return ( 
     <div className="app-container">
-      <img src={logo} alt="Haventrace Logo" className="app-logo" onClick={navigateToWelcome} />
+      <img src={logo} alt="Haventrace Logo" className="app-logo" onClick={navigateToHomePage}/>
     </div>
   );
 }
+
+
 
 export default App;
