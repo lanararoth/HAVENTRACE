927e250e21237496998e6a72fd52e8b95d301dec
lanararoth <165750255+lanararoth@users.noreply.github.com>
2025-02-04 02:15:20 +0530

    Add files via upload

---
+++ frontend/src/pages/EmergencyNumbers.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import emergencyNumbers from "../assets/data/Emergency";
+import "../styles/EmergencyNumbers.css";
+
+const EmergencyNumbers = () => {
+  return (
+    <div className="emergency-numbers-container">
+      <h1 className="title">Emergency Contacts</h1>
+      <div className="cardscontainer">
+        {emergencyNumbers.map((item) => (
+          <div key={item.id} className="cards">
+            <div className="icon">{item.icon}</div>
+            <h2 className="service">{item.service}</h2>
+            <p className="number">{item.number}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default EmergencyNumbers;
