37b026cab29d0fe93366b7ebc2d23792e4739175
AyshaNidapp <165902463+AyshaNidapp@users.noreply.github.com>
2025-02-04 11:57:46 +0530

    Add files via upload

---
+++ backend/number/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+# models.py
+
+from django.db import models
+
+class EmergencyContact(models.Model):
+    service_name = models.CharField(max_length=255)
+    contact_number = models.CharField(max_length=15)
+
+    def __str__(self):
+        return f"{self.service_name}: {self.contact_number}"
---
+++ backend/number/serializers.py
@@ -0,0 +1,9 @@
+# serializers.py
+
+from rest_framework import serializers
+from .models import EmergencyContact
+
+class EmergencyContactSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = EmergencyContact
+        fields = ['id', 'service_name', 'contact_number']
